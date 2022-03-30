0335f47cae57c5f5debb74a1bf10cb818754200c
MateusProfissa <94137703+MateusProfissa@users.noreply.github.com>
2022-03-30 14:43:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Meu primeiro projeto no Console")
+
+var nome1 = "Usuário 1";
+var nome2 = "Usuário 2";
+var nomes = nome1.concat(nome2);
+console.log(nomes);
+
+var número_x = 12;
+var número_y = 6;
+
+var soma = número_x + número_y;
+var subtração = número_x - número_y;
+var multiplicação = número_x * número_y;
+var divisão = número_x / número_y;
+console.log(soma, subtração, multiplicação, divisão);
